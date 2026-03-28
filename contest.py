6080daf35212f822db1f9b9fdfeb052fdbb929c8
di593488-sys <di593488@gmail.com>
2026-03-28 15:50:58 +0900

    Update contest.py

--- contest.py
+++ contest.py
@@ -10,7 +10,10 @@ LIST_URL = "https://www.wevity.com/?c=find&s=1&gub=1"
 BASE_URL = "https://www.wevity.com/"
 
 HEADERS = {
-    "User-Agent": "Mozilla/5.0"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+    "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Referer": "https://www.wevity.com/",
+    "Connection": "keep-alive"
 }
 
 
